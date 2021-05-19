44de8403fb252896c3507eded1c84f5c2efcc10a
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:42:57 +0200

    Add files via upload

---
+++ SelectionProblems/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace SelectionProblems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSizeSelector = new System.Windows.Forms.Button();
+            this.BtnPassOrFail = new System.Windows.Forms.Button();
+            this.BtnGrading = new System.Windows.Forms.Button();
+            this.BtnGasRates = new System.Windows.Forms.Button();
+            this.BtnTelegram = new System.Windows.Forms.Button();
+            this.BtnElectricityRates = new System.Windows.Forms.Button();
+            this.BtnTaxBracket = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnSizeSelector
+            // 
+            this.BtnSizeSelector.Location = new System.Drawing.Point(12, 12);
+            this.BtnSizeSelector.Name = "BtnSizeSelector";
+            this.BtnSizeSelector.Size = new System.Drawing.Size(193, 34);
+            this.BtnSizeSelector.TabIndex = 0;
+            this.BtnSizeSelector.Text = "Small, Medium and Large";
+            this.BtnSizeSelector.UseVisualStyleBackColor = true;
+            this.BtnSizeSelector.Click += new System.EventHandler(this.BtnSizeSelector_Click);
+            // 
+            // BtnPassOrFail
+            // 
+            this.BtnPassOrFail.Location = new System.Drawing.Point(12, 52);
+            this.BtnPassOrFail.Name = "BtnPassOrFail";
+            this.BtnPassOrFail.Size = new System.Drawing.Size(193, 34);
+            this.BtnPassOrFail.TabIndex = 1;
+            this.BtnPassOrFail.Text = "Pass or Fail";
+            this.BtnPassOrFail.UseVisualStyleBackColor = true;
+            this.BtnPassOrFail.Click += new System.EventHandler(this.BtnPassOrFail_Click);
+            // 
+            // BtnGrading
+            // 
+            this.BtnGrading.Location = new System.Drawing.Point(12, 92);
+            this.BtnGrading.Name = "BtnGrading";
+            this.BtnGrading.Size = new System.Drawing.Size(193, 34);
+            this.BtnGrading.TabIndex = 2;
+            this.BtnGrading.Text = "Grading ";
+            this.BtnGrading.UseVisualStyleBackColor = true;
+            this.BtnGrading.Click += new System.EventHandler(this.BtnGrading_Click);
+            // 
+            // BtnGasRates
+            // 
+            this.BtnGasRates.Location = new System.Drawing.Point(12, 132);
+            this.BtnGasRates.Name = "BtnGasRates";
+            this.BtnGasRates.Size = new System.Drawing.Size(193, 34);
+            this.BtnGasRates.TabIndex = 3;
+            this.BtnGasRates.Text = "Gas Rates";
+            this.BtnGasRates.UseVisualStyleBackColor = true;
+            this.BtnGasRates.Click += new System.EventHandler(this.BtnGasRates_Click);
+            // 
+            // BtnTelegram
+            // 
+            this.BtnTelegram.Location = new System.Drawing.Point(12, 172);
+            this.BtnTelegram.Name = "BtnTelegram";
+            this.BtnTelegram.Size = new System.Drawing.Size(193, 34);
+            this.BtnTelegram.TabIndex = 4;
+            this.BtnTelegram.Text = "Telegram Word Cost";
+            this.BtnTelegram.UseVisualStyleBackColor = true;
+            this.BtnTelegram.Click += new System.EventHandler(this.BtnTelegram_Click);
+            // 
+            // BtnElectricityRates
+            // 
+            this.BtnElectricityRates.Location = new System.Drawing.Point(12, 212);
+            this.BtnElectricityRates.Name = "BtnElectricityRates";
+            this.BtnElectricityRates.Size = new System.Drawing.Size(193, 34);
+            this.BtnElectricityRates.TabIndex = 5;
+            this.BtnElectricityRates.Text = "Electricity Rates";
+            this.BtnElectricityRates.UseVisualStyleBackColor = true;
+            this.BtnElectricityRates.Click += new System.EventHandler(this.BtnElectricityRates_Click);
+            // 
+            // BtnTaxBracket
+            // 
+            this.BtnTaxBracket.Location = new System.Drawing.Point(12, 252);
+            this.BtnTaxBracket.Name = "BtnTaxBracket";
+            this.BtnTaxBracket.Size = new System.Drawing.Size(193, 34);
+            this.BtnTaxBracket.TabIndex = 6;
+            this.BtnTaxBracket.Text = "Tax Bracket Placer";
+            this.BtnTaxBracket.UseVisualStyleBackColor = true;
+            this.BtnTaxBracket.Click += new System.EventHandler(this.BtnTaxBracket_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 299);
+            this.Controls.Add(this.BtnTaxBracket);
+            this.Controls.Add(this.BtnElectricityRates);
+            this.Controls.Add(this.BtnTelegram);
+            this.Controls.Add(this.BtnGasRates);
+            this.Controls.Add(this.BtnGrading);
+            this.Controls.Add(this.BtnPassOrFail);
+            this.Controls.Add(this.BtnSizeSelector);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSizeSelector;
+        private System.Windows.Forms.Button BtnPassOrFail;
+        private System.Windows.Forms.Button BtnGrading;
+        private System.Windows.Forms.Button BtnGasRates;
+        private System.Windows.Forms.Button BtnTelegram;
+        private System.Windows.Forms.Button BtnElectricityRates;
+        private System.Windows.Forms.Button BtnTaxBracket;
+    }
+}
+
---
+++ SelectionProblems/Form1.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace SelectionProblems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Write a program which will accept as input a persons show size and then make a comment depending on the size. Use 
+         * the following three size categories: 
+         * a) size 4 and under
+         * b) between 5 and 9
+         * c) greater than 9
+         */
+        private void BtnSizeSelector_Click(object sender, EventArgs e)
+        {
+            int size;
+
+            size = Convert.ToInt32(Interaction.InputBox("Please enter your size.", "Size", ""));
+
+            if (size <= 4)
+            {
+                MessageBox.Show("Your size is a Small");
+            }
+            else if (size >= 5 && size <= 9)
+            {
+                MessageBox.Show("Your size is Medium");
+            }
+            else
+            {
+                MessageBox.Show("Your size is a Large");
+            }
+        }
+
+        /*
+         * Write a program which will accept 4 marks and determine the average. Once the average is determined the program
+         * should display either “You have a passing grade” or “Sorry you have a failing grade”
+         */
+        private void BtnPassOrFail_Click(object sender, EventArgs e)
+        {
+            float mark1;
+            float mark2;
+            float mark3;
+            float mark4;
+            float average;
+
+            mark1 = float.Parse(Interaction.InputBox("Please enter your first mark", "Mark one", ""), CultureInfo.InvariantCulture);
+            mark2 = float.Parse(Interaction.InputBox("Please enter your second mark", "Mark one", ""), CultureInfo.InvariantCulture);
+            mark3 = float.Parse(Interaction.InputBox("Please enter your third mark", "Mark one", ""), CultureInfo.InvariantCulture);
+            mark4 = float.Parse(Interaction.InputBox("Please enter your fourth mark", "Mark one", ""), CultureInfo.InvariantCulture);
+
+            average = (mark1 + mark2 + mark3 + mark4) / 4;
+
+            if (average > 49)
+            {
+                MessageBox.Show("You have a passing grade.");
+            }
+            else
+            {
+                MessageBox.Show("You have a failing grade");
+            }
+
+        }
+
+        /*
+         * Write a program which accepts a mark and then determines the letter grade to assign it. Letter grades are assigned follows:
+         * 0 <= mark < 50 F
+         * 50 <= mark < 60 D
+         * 60 <= mark < 70 C
+         * 70 <= mark < 80 B
+         * 80 <= mark < 100 A
+         */
+        private void BtnGrading_Click(object sender, EventArgs e)
+        {
+            int mark;
+
+            mark = Convert.ToInt32(Interaction.InputBox("Please enter your mark", "", ""));
+
+            if (mark < 50)
+            {
+                MessageBox.Show("Your Grade is a F.");
+            }
+            else if (mark <= 50 && mark < 60)
+            {
+                MessageBox.Show("Your Grade is a D.");
+            }
+            else if (mark <= 60 && mark < 70)
+            {
+                MessageBox.Show("Your mark is a C.");
+            }
+            else if (mark <= 70 && mark < 80)
+            {
+                MessageBox.Show("Your mark is a B.");
+            }
+            else
+            {
+                MessageBox.Show("Your mark is an A.");
+            }
+        }
+
+        /*
+         * A gas company measures the amount of gas its customers use in hundreds of cubic feet (ccf) and charges them
+         * according to the following schedule:
+         * first 8 ccf 50 cents each
+         * more than 8 ccf 30 cents each
+         * Note that the 30 cent rate applies only to usage above 8 ccf. A customer who uses 10 ccf is charged 50 cents for the
+         * first 8 ccf and 30 cents for the next 2 ccf. Write a program to calculate customer’s bills.
+         */
+        private void BtnGasRates_Click(object sender, EventArgs e)
+        {
+            int amount;
+            int remaining;
+
+            amount = Convert.ToInt32(Interaction.InputBox("Please enter your amount used.", "", ""));
+
+            if (amount <= 8)
+            {
+                amount *= 50;
+            }
+            else
+            {
+                remaining = amount - 8;
+                remaining *= 30;
+
+                amount = (8 * 50) + remaining;
+            }
+            MessageBox.Show("Your bill this month was R" + amount);
+
+        }
+
+        /*
+         * Assume that the cost, not including tax, for sending a telegram from Hamilton to Montreal is $4.50 for the first twenty
+         * words or less, plus $.17 for each additional word beyond twenty. Write a program which accepts the number of words
+         * and then determines the total cost.
+         */
+        private void BtnTelegram_Click(object sender, EventArgs e)
+        {
+            float baseCost = 4.50f;
+            float extraWordCost = 0.17f;
+            int numberOfWords;
+            float remaining;
+            float totalCost = 0;
+
+            numberOfWords = Convert.ToInt32(Interaction.InputBox("Please enter the amount of words.", "Word Calculator", ""));
+
+            if (numberOfWords <= 20)
+            {
+                totalCost = baseCost * numberOfWords;
+            }
+            else
+            {
+                remaining = numberOfWords - 20;
+                remaining *= extraWordCost;
+
+                totalCost = (baseCost * 20) + remaining;
+            }
+            MessageBox.Show("The Total Cost for this Telegram is R" + totalCost);
+
+        }
+
+        /*
+         * An electric company measures the amount of electricity its customer use in kilowatt hours (kWh) and charges them
+         * according to the following schedule:
+         * first 12 kWh or less $2.80
+         * next 78 kWh 8 cents each kWh
+         * excess above 90 kWh 6 cents each kWh
+         * Note that the minimum bill is $2.80. Write a program to calculate a customers bill.
+         */
+        private void BtnElectricityRates_Click(object sender, EventArgs e)
+        {
+            int amountOfkWh;
+            float remaining;
+            float bill = 0;
+
+            amountOfkWh = Convert.ToInt32(Interaction.InputBox("Please enter the amount of kWh.", "kWh Calculator.", ""));
+
+            if (amountOfkWh <= 12)
+            {
+                bill = 2.80f;
+            }
+            else if (amountOfkWh > 12 && amountOfkWh <= 90)
+            {
+                bill = 2.80f;
+                remaining = (amountOfkWh - 12) * 0.8f;
+                bill += remaining;
+            }
+            else if (amountOfkWh > 90)
+            {
+                bill = 2.80f + (78 * 0.80f) + ((amountOfkWh - 90) * 0.06f);
+            }
+
+            MessageBox.Show("Your bill is R" + bill);
+        }
+
+        /*
+         * Write a program which determines the amount of Federal Income Tax one owes when given the taxable income.
+         * Federal tax is determined as follows:
+         * Taxable Income                                                   Tax
+         * $1295 or less                                                    6%
+         * greater than 1295 and less than or equal to 2590                 $78 + 16% on next 1295
+         * greater than 2590 and less than or equal to 5180                 $285 + 17% on next 2590
+         * greater than 5180 and less than or equal to 7770                 $725 + 18% on next 2590
+         * greater than 7770 and less than or equal to 12950                $1191 + 19% on next 5180
+         * greater than 12950 and less than or equal to 18130               $2176 + 20% on next 5180
+         * greater than 18130 and less than or equal to 23310               $3212 + 23% on next 5180
+         * greater than 23310 and less than or equal to 36260               $4403 + 25% on next 12950
+         * greater than 36260 and less than or equal to 62160               $7641 + 30% on next 25900
+         * greater than 62160                                               $15411 + 34% on remainder
+         */
+        private void BtnTaxBracket_Click(object sender, EventArgs e)
+        {
+            float grossIncome = 0;
+            float tax = 0;
+
+            grossIncome = float.Parse(Interaction.InputBox("Please enter your gross income.", "Tax Calculator.", ""), CultureInfo.InvariantCulture);
+
+            if (grossIncome <= 1295)
+                tax = grossIncome * 0.06f;
+
+            else if (grossIncome > 1295 && grossIncome <= 2590)
+                tax = ((grossIncome - 1295) * 0.16f) + 78;
+
+            else if (grossIncome > 2590 && grossIncome <= 5180)
+                tax = ((grossIncome - 2590) * 0.17f) + 285;
+
+            else if (grossIncome > 5180 && grossIncome <= 7770)
+                tax = ((grossIncome - 2590) * 0.18f) + 725;
+
+            else if (grossIncome > 7770 && grossIncome <= 12950)
+                tax = ((grossIncome - 5180) * 0.19f) + 1191;
+
+            else if (grossIncome > 12950 && grossIncome <= 18130)
+                tax = ((grossIncome - 5180) * 0.20f) + 2176;
+
+            else if (grossIncome > 18130 && grossIncome <= 23310)
+                tax = ((grossIncome - 5180) * 0.23f) + 3212;
+
+            else if (grossIncome > 23310 && grossIncome <= 36260)
+                tax = ((grossIncome - 12950) * 0.25f) + 4403;
+
+            else if (grossIncome > 36260 && grossIncome <= 62160)
+                tax = ((grossIncome - 25900) * 0.30f) + 7641;
+
+            else
+                tax = ((grossIncome - 25900) * 0.34f) + 15411;
+
+            MessageBox.Show("The amount of tax owed on R" + grossIncome + " is R" + tax);
+        }
+    }
+}
